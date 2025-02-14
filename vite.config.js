15b3370b1ff32e3c51f1250db2bde7fe7cb2e925
Server User <server@example.com>
2025-02-14 09:04:06 +0000

    Regenerated composer.lock after resolving merge conflict

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,22 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+   plugins: [
+       laravel({
+           input: ['resources/css/app.css', 'resources/js/app.js'],
+           refresh: true,
+       }),
+   ],
+   build: {
+       outDir: 'public/build',
+       emptyOutDir: true,
+       chunkSizeWarningLimit: 1600,
+       rollupOptions: {
+           output: {
+               manualChunks: {
+                   vendor: []
+               }
+           }
+       }
+   }
 });
