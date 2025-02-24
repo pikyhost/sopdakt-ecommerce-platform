425c43062f27d3a78199ab2af2ca27e62fc5801f
Mohamed Elsheshtawy <ramyalfe22@gmail.com>
2025-02-24 03:16:14 +0200

    solve from 1 to 8 points 6

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -32,14 +32,15 @@ class AdminPanelProvider extends PanelProvider
 {
     public function panel(Panel $panel): Panel
     {
-        // Retrieve site settings from the database (with a default fallback)
-        $settings = Setting::getSetting('site_settings') ?? [];
-        
-        // Ensure favicon settings exist
-        $favicon = isset($settings['favicon']['en'])
-            ? asset('storage/' . $settings['favicon']['en'])  // Get favicon from storage
-            : asset('images/clients/client1.png');  // Default fallback favicon
-
+//        // Retrieve site settings from the database (with a default fallback)
+//        $settings = Setting::getSetting('site_settings') ?? [];
+//
+//
+//        // Ensure favicon settings exist
+//        $favicon = isset($settings['favicon']['en'])
+//            ? asset('storage/' . $settings['favicon']['en'])  // Get favicon from storage
+//            : asset('images/clients/client1.png');  // Default fallback favicon
+//
 
         return $panel
             ->default()
