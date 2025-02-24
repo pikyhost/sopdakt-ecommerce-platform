fc30a3a5603f347c913d046273734601d5358003
Mohamed Elsheshtawy <ramyalfe22@gmail.com>
2025-02-24 16:22:35 +0200

    solve from 1 to 17 points00

--- routes/web.php
+++ routes/web.php
@@ -17,13 +17,14 @@ Route::get('/category-page', function () {
 });
 
 Route::redirect('/admin/settings', '/admin/settings/1/edit');
-Route::get('/category/{slug}', [CategoryProductController::class, 'show'])->name('category.products');
+
 
 Route::group(['prefix' => LaravelLocalization::setLocale(), 'middleware' => ['localeSessionRedirect', 'localizationRedirect', 'localeViewPath']], function () {
     Route::post('landing-pages/{id}/show-purchase-form', [LandingPageController::class, 'saveBundleData'])->name('landing-pages.purchase-form.save-bundle-data');
     Route::get('landing-pages/{slug}/show-purchase-form', [LandingPageController::class, 'showPurchaseForm'])->name('landing-pages.purchase-form.show');
     Route::get('landing-page/{slug}', [LandingPageController::class, 'show'])->name('landing-page.show-by-slug');
     Route::get('/products/{slug}', [ProductController::class, 'show'])->name('product.show');
+    Route::get('/category/{slug}', [CategoryProductController::class, 'show'])->name('category.products');
     Route::get('/regions', [RegionsController::class, 'index'])->name('regions.index');
     Route::post('/calculate-shipping', [ShippingController::class, 'calculateShipping'])->name('shipping.calculate');
     Route::post('/landing-pages/{id}/get-combination-price', [LandingPageController::class, 'getCombinationPrice'])->name('dashboard.landing-pages.get-combination-price');
