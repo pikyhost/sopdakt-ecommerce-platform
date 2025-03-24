1213149202c287e82cfa28747a98a700b8535602
Mohamed Elsheshtawy <ramyalfe22@gmail.com>
2025-03-24 18:49:44 +0200

    New new0

--- app/Mail/OrderStatusMail.php
+++ app/Mail/OrderStatusMail.php
@@ -7,8 +7,6 @@ use App\Enums\OrderStatus;
 use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Mail\Mailables\Address;
-use Illuminate\Mail\Mailables\Envelope;
 
 class OrderStatusMail extends Mailable
 {
@@ -26,27 +24,15 @@ class OrderStatusMail extends Mailable
         $this->status = $status;
     }
 
-    /**
-     * Get the message envelope.
-     */
-    public function envelope(): Envelope
-    {
-        return new Envelope(
-            from: new Address('xeno@example.com', 'Xeno'),
-            subject: 'Test',
-        );
-    }
-
     public function build()
     {
-        // Get a more user-friendly order status message
-        $statusMessage = __('messages.order_status_' . $this->status->value);
-
         return $this->subject(__('Order') . ' #' . $this->order->id . ' - ' . __($this->status->getLabel()))
             ->view('emails.order-status')
             ->with([
                 'order' => $this->order,
-                'statusMessage' => $statusMessage,
+                'orderItems' => $this->order->items,
+                'statusMessage' => __('messages.order_status_' . $this->status->value),
             ]);
     }
+
 }
