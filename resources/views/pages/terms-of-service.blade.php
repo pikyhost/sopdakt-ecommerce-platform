0b1213e1ad7d55d7d36bdc9371ca2a660e67ec32
Mohamed Elsheshtawy <ramyalfe22@gmail.com>
2025-03-26 22:19:00 +0200

    New new0000

--- resources/views/pages/terms-of-service.blade.php
+++ resources/views/pages/terms-of-service.blade.php
@@ -4,11 +4,14 @@
     $direction = app()->getLocale() === 'ar' ? 'rtl' : 'ltr';
 @endphp
 
-@section('title', __('My shopping cart'))
+@section('title', __('policy.privacy_policy'))
 
 @section('content')
     <br>
     <div class="container text-center" dir="{{ $direction }}">
-
+        <h1>{{ __('policy.privacy_policy') }}</h1>
+        <div class="content">
+            {!! \App\Models\Policy::first()?->{"terms_of_service_" . app()->getLocale()} ?? __('policy.no_policy_found') !!}
+        </div>
     </div>
 @endsection
