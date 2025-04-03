f8df7fc2b5720a608627a20e069de17a2a23406f
Mohamed Elsheshtawy <ramyalfe22@gmail.com>
2025-04-03 20:45:01 +0200

    done

--- resources/views/pages/blogs.blade.php
+++ resources/views/pages/blogs.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
     <br>
-    <div class="container text-center" dir="{{ $direction }}">
+    <div class="container text-center">
         <h1>{{ __('Blogs') }}</h1>
         <div class="content">
             <p>Welcome to the Blogs page!</p>
--- resources/views/pages/products.blade.php
+++ resources/views/pages/products.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
     <br>
-    <div class="container text-center" dir="{{ $direction }}">
+    <div class="container text-center">
         <h1>{{ __('Products') }}</h1>
         <div class="content">
             <p>Welcome to the Products page!</p>
