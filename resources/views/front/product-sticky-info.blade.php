6a3d2e14e3b52933e1c50de4e458ae78427bdf77
Mohamed Elsheshtawy <ramyalfe22@gmail.com>
2025-04-04 16:48:51 +0200

    done

--- resources/views/front/product-sticky-info.blade.php
+++ resources/views/front/product-sticky-info.blade.php
@@ -6,8 +6,8 @@
     <div class="container">
         <nav aria-label="breadcrumb" class="breadcrumb-nav">
             <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href="demo4.html"><i class="icon-home"></i></a></li>
-                <li class="breadcrumb-item"><a href="#">Products</a></li>
+                <li class="breadcrumb-item"><a href="{{ url('/') }}"><i class="icon-home"></i></a></li>
+                <li class="breadcrumb-item"><a href="{{ url('/products') }}">Products</a></li>
             </ol>
         </nav>
 
