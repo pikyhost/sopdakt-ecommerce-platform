d036acc1dd5cddfde56c1fb27b2dff36cb9be03a
Mohamed Elsheshtawy <ramyalfe22@gmail.com>
2025-04-06 20:26:48 +0200

    last

--- resources/views/filament/pages/env-editor.blade.php
+++ resources/views/filament/pages/env-editor.blade.php
@@ -1,4 +1,9 @@
 <x-filament::page>
+    <x-filament::callout color="danger" icon="heroicon-o-exclamation-triangle" class="mb-6">
+        <strong>{{ __('env.warning.title') }}</strong><br>
+        {{ __('env.warning.body') }}
+    </x-filament::callout>
+
     {{ $this->form }}
 
     <x-filament::button wire:click="save" class="mt-4" color="primary">
