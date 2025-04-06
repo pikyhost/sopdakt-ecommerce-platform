f222241314927cae54be710bf1bfacba93bfc637
Mohamed Elsheshtawy <ramyalfe22@gmail.com>
2025-04-06 19:53:33 +0200

    last

--- resources/views/filament/pages/env-editor.blade.php
+++ resources/views/filament/pages/env-editor.blade.php
@@ -2,6 +2,6 @@
     {{ $this->form }}
 
     <x-filament::button wire:click="save" class="mt-4" color="primary">
-        Save Changes
+        {{ __('Save') }}
     </x-filament::button>
 </x-filament::page>
