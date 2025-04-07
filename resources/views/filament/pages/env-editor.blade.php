3e3cf4d3ee1a5de307f868e594f9aaba13e980d5
Mohamed Elsheshtawy <ramyalfe22@gmail.com>
2025-04-07 20:46:41 +0200

    last

--- resources/views/filament/pages/env-editor.blade.php
+++ resources/views/filament/pages/env-editor.blade.php
@@ -1,7 +1,6 @@
 <x-filament::page>
     <div class="p-4 mb-6 text-red-800 border border-red-300 rounded-lg bg-red-50 dark:bg-red-950 dark:text-red-200 dark:border-red-800">
         <div class="flex items-center mb-2 font-semibold">
-
             {{ __('env.warning.title') }}
         </div>
         <p>{{ __('env.warning.body') }}</p>
@@ -9,7 +8,13 @@
 
     {{ $this->form }}
 
-    <x-filament::button wire:click="save" class="mt-4" color="primary">
-        {{ __('Save') }}
-    </x-filament::button>
+    <div class="flex gap-4 mt-4">
+        <x-filament::button wire:click="save" color="primary">
+            {{ __('Save changes') }}
+        </x-filament::button>
+
+        <x-filament::button tag="a" href="{{ url()->previous() }}" color="gray">
+            {{ __('Cancel') }}
+        </x-filament::button>
+    </div>
 </x-filament::page>
