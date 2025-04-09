d08b79e69d554242a453f0a3fe6af0c6f11cced6
Mohamed Elsheshtawy <ramyalfe22@gmail.com>
2025-04-09 13:28:50 +0200

    last

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -2,14 +2,18 @@
 
 namespace App\Filament\Pages;
 
+use App\Filament\Widgets\RedirectHomePage;
 use App\Livewire\DashboardFilter;
 use Filament\Pages\Dashboard as BaseDashboard;
+use Filament\Widgets\AccountWidget;
 
 class Dashboard extends BaseDashboard
 {
     protected function getHeaderWidgets(): array
     {
         return [
+            AccountWidget::class,
+            RedirectHomePage::class,
             DashboardFilter::class,
         ];
     }
