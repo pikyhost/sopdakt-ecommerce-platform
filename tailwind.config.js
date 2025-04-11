a51360cdc4b2cf3269084b5023c2e936a3ed96c7
Mohamed Elsheshtawy <ramyalfe22@gmail.com>
2025-04-11 08:06:56 +0200

    done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
+import defaultTheme from 'tailwindcss/defaultTheme'
 
 /** @type {import('tailwindcss').Config} */
 export default {
+    darkMode: 'class',
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
@@ -17,4 +18,4 @@ export default {
         },
     },
     plugins: [],
-};
+}
