2300fea9b093aebe38bd53b90413bbdc222b819a
Mohamed Elsheshtawy <ramyalfe22@gmail.com>
2025-04-19 09:49:16 +0200

    Done

---
+++ database/migrations/2025_04_19_074807_add_is_active_to_discounts_and_coupons_tables.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('discounts', function (Blueprint $table) {
+            $table->boolean('is_active')->default(true);
+        });
+
+        Schema::table('coupons', function (Blueprint $table) {
+            $table->boolean('is_active')->default(true);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('discounts_and_coupons_tables', function (Blueprint $table) {
+            //
+        });
+    }
+};
