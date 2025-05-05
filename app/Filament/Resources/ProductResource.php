f3b30f99a8c5bc43ae5e5d54326cbc3d864b15fc
Mohamed Elsheshtawy <ramyalfe22@gmail.com>
2025-05-05 09:29:01 +0300

    ok

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -118,6 +118,12 @@ class ProductResource extends Resource
                                     ->label(__('Slug'))
                                     ->required()
                                     ->maxLength(255),
+
+                                Forms\Components\Checkbox::make('must_be_collection')
+                                    ->columnSpanFull()
+                                    ->label(__('Must be Collection?'))
+                                    ->helperText(__('This product must be added to the cart with a quantity greater than 2.')),
+
                             ]),
 
                         // Pricing & Stock Tab
