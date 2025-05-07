4d944c6a703b75d462167fad490b6335db99608d
Mohamed Elsheshtawy <ramyalfe22@gmail.com>
2025-05-07 10:09:14 +0300

    done main

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -4,15 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 use Spatie\Translatable\HasTranslations;
 
-class Blog extends Model
+class Blog extends Model implements HasMedia
 {
-    use HasFactory, HasTranslations;
+    use HasFactory, HasTranslations, InteractsWithMedia;
 
     protected $guarded = [];
 
     public $translatable = ['title', 'content'];
+
+    protected $casts = [
+        'published_at' => 'datetime'
+    ];
 
     public function category()
     {
