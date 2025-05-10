dd119d7d2653b353cffdc7d328ed007c617139d5
Mohamed Elsheshtawy <ramyalfe22@gmail.com>
2025-05-10 15:24:13 +0300

    done

--- app/Enums/OrderStatus.php
+++ app/Enums/OrderStatus.php
@@ -2,10 +2,13 @@
 
 namespace App\Enums;
 
+use Filament\Support\Contracts\HasColor;
+use Filament\Support\Contracts\HasIcon;
 use Filament\Support\Contracts\HasLabel;
 
-enum OrderStatus: string implements HasLabel
+enum OrderStatus: string implements HasColor, HasIcon, HasLabel
 {
+    case Pending = 'Pending';
     case Preparing = 'Preparing';
     case Shipping = 'Shipping';
     case Completed = 'Completed';
@@ -13,8 +16,44 @@ enum OrderStatus: string implements HasLabel
     case Cancelled = 'Cancelled';
     case Delayed = 'Delayed';
 
-    public function getLabel(): ?string
+    public function getLabel(): string
     {
-        return $this->value;
+        return match ($this) {
+            self::Pending   => __('Pending'),
+            self::Preparing => __('Preparing'),
+            self::Shipping  => __('Shipping'),
+            // confirmed
+            self::Delayed   => __('Delayed'),
+            self::Refund    => __('Refund'),
+            self::Cancelled => __('Cancelled'),
+            self::Completed => __('Completed'),
+        };
+    }
+
+    public function getColor(): string | array | null
+    {
+        return match ($this) {
+            self::Pending   => 'info',
+            self::Preparing => 'warning',
+            self::Shipping  => 'primary',
+            self::Delayed   => 'danger',
+            self::Refund    => 'gray',
+            self::Completed => 'success',
+            default => 'danger',
+        };
+    }
+
+    public function getIcon(): ?string
+    {
+        return match ($this) {
+            self::Pending   => 'heroicon-m-clock',
+            self::Preparing => 'heroicon-m-cog',
+            self::Shipping  => 'heroicon-m-truck',
+            self::Delayed   => 'heroicon-m-exclamation-circle',
+            self::Refund    => 'heroicon-m-arrow-uturn-left',
+            self::Cancelled => 'heroicon-o-x-circle',
+            self::Completed => 'heroicon-m-check-badge',
+        };
     }
 }
+
