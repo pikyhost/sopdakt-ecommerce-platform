3b77fdde945672d473d1cd9a8dd47cfca888d31e
Mohamed Elsheshtawy <ramyalfe22@gmail.com>
2025-05-16 15:02:39 +0300

    Temporary commit for local changes

--- app/Http/Controllers/Api/ContactSettingController.php
+++ app/Http/Controllers/Api/ContactSettingController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\ContactSetting;
+use App\Models\Setting;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Log;
 
