5e96d6860d45adfa4fc48ea9fd331a4f8c5cd884
Mohamed Elsheshtawy <ramyalfe22@gmail.com>
2025-05-16 12:43:50 +0300

    Temporary commit for local changes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,16 +12,16 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->validateCsrfTokens(except: [
             'jt-express-webhook',
             '/api/jt-express-webhook',
-              '/bosta/webhook',
+            '/bosta/webhook',
             '/api/bosta/webhook',
         ]);
 
@@ -32,6 +32,15 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
             'protect.docs' => \App\Http\Middleware\ProtectDocs::class,
+        ]);
+
+        $middleware->alias([
+            /**** OTHER MIDDLEWARE ALIASES ****/
+            'localize'                => \Mcamara\LaravelLocalization\Middleware\LaravelLocalizationRoutes::class,
+            'localizationRedirect'    => \Mcamara\LaravelLocalization\Middleware\LaravelLocalizationRedirectFilter::class,
+            'localeSessionRedirect'   => \Mcamara\LaravelLocalization\Middleware\LocaleSessionRedirect::class,
+            'localeCookieRedirect'    => \Mcamara\LaravelLocalization\Middleware\LocaleCookieRedirect::class,
+            'localeViewPath'          => \Mcamara\LaravelLocalization\Middleware\LaravelLocalizationViewPath::class,
         ]);
 
         $middleware->prependToGroup('api', \App\Http\Middleware\AlwaysAcceptJson::class);
@@ -87,7 +96,7 @@ return Application::configure(basePath: dirname(__DIR__))
             ], 405);
         });
 
-         $exceptions->renderable(function (ModelNotFoundException $e) {
-             return response()->json(['message' => 'Object not found.'], 404);
-         });
+        $exceptions->renderable(function (ModelNotFoundException $e) {
+            return response()->json(['message' => 'Object not found.'], 404);
+        });
     })->create();
