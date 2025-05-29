e7686bc910ac72e71e796c56b2663b30e474d8c4
Mohamed Elsheshtawy <ramyalfe22@gmail.com>
2025-05-29 12:10:43 +0300

    done

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -29,7 +29,7 @@ return Application::configure(basePath: dirname(__DIR__))
 
         // API middleware configuration
         $middleware->api(prepend: [
-//            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             HandleCors::class,
         ]);
 
