8ad8f0f9d7403864dca8da9e8a0281f3663765dd
Mohamed Elsheshtawy <ramyalfe22@gmail.com>
2025-06-14 21:29:12 +0300

    Google login

--- app/Http/Controllers/Api/Auth/GoogleAuthController.php
+++ app/Http/Controllers/Api/Auth/GoogleAuthController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
 
 class GoogleAuthController extends Controller
 {
