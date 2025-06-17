54feb0656ab16a6a5da06b7dd92452a345f6edcc
Mohamed Elsheshtawy <ramyalfe22@gmail.com>
2025-06-17 23:24:59 +0300

    Reset password custom notification added

--- app/Notifications/CustomResetPassword.php
+++ app/Notifications/CustomResetPassword.php
@@ -15,10 +15,10 @@ class CustomResetPassword extends ResetPassword
         ], false)); // false to avoid double domain prefix
 
         return (new MailMessage)
-            ->subject('Reset Password Notification')
-            ->line('You are receiving this email because we received a password reset request for your account.')
-            ->action('Reset Password', $resetUrl)
-            ->line('This password reset link will expire in 60 minutes.')
-            ->line('If you did not request a password reset, no further action is required.');
+            ->subject('Reset Password Notification updated')
+            ->line('updated You are receiving this email because we received a password reset request for your account.')
+            ->action(' updated Reset Password', $resetUrl)
+            ->line('updated This password reset link will expire in 60 minutes.')
+            ->line('updated If you did not request a password reset, no further action is required.');
     }
 }
